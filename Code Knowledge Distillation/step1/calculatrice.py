4ee7cd049c5c9f8a6023277839ea08041df56a9a
NicolasThou <ncls.thou@gmail.com>
2020-05-11 20:47:08 +0200

    Nico

--- Code Knowledge Distillation/step1/calculatrice.py
+++ Code Knowledge Distillation/step1/calculatrice.py
@@ -5,6 +5,7 @@ from mxnet.ndarray import *
 
 if __name__ == '__main__':
 
+    """
     pred_student = nd.random.uniform(-1, 1, (2, 5, 4))
     target = nd.random.uniform(-1, 1, (2, 5, 4))
     pred_teacher = nd.random.uniform(-1, 1, (2, 5, 4))
@@ -31,3 +32,9 @@ if __name__ == '__main__':
 
     result = mean(result, axis=1)
     print(result)
+    """
+    x = nd.random.randint(-10, 10, (2,3,4,5))
+    print(x)
+    print(x[1,:,3,4])
+    print(nd.sum(x, axis=1))
+
